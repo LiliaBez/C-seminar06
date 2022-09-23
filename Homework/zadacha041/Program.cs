ebfa6655c7afc7b3d1196a1a34d734b1d66dc1bc
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-23 20:12:12 +0400

    Домашнее задание

---
+++ Homework/zadacha041/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+Console.WriteLine("Введите числа через запятую:");
+
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+
+Console.WriteLine($"Количество чисел больше 0: {CountNumberPositiv(numbers)}");
+
+
+
+int CountNumberPositiv(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+
+
---
+++ Homework/zadacha043/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая найдёт точку 
+пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.*/
+
+/*Вероятно, как то сюда нужно вписать двумерные массивы
+и рекурсию... или хотя бы функции, но что-то не соображу..*/
+
+
+Console.WriteLine("Введите через запятую значения b1, k1 и b2, k2 ");
+
+double[] array = Array.ConvertAll(Console.ReadLine().Split(','), double.Parse);
+
+double firstPoint = (array[2] - array[0]) / (array[1] - array[3]);
+double secondPoint = array[1] * firstPoint + array[0];
+
+Console.WriteLine($"Координаты точки пересечения прямых: ({firstPoint}, {secondPoint})");
+
+
+
